a24980f1e349b7c5d81a4974ef796f5a1baebb02
Chris Ward <tjcw@cantab.net>
2019-12-12 18:38:49 +0000

    fix wrapper program

--- packaging/crunqemu.c
+++ packaging/crunqemu.c
@@ -3,19 +3,25 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
+
+enum {
+  k_Debug = 0
+};
 int main(int argc, char **argv)
 {
   char buffer[1000] ; 
-//  printf("argv[0] is %s argc is %d\n", argv[0], argc) ; 
-// if ( argc == 1 )
-  {
-//  printf("argv[1] is %s\n", argv[1]) ; 
-//  fflush(stdout) ;
+  if ( k_Debug ) {
+    printf("argv[0] is %s argc is %d\n", argv[0], argc) ;
+    printf("argv[1] is %s\n", argv[1]) ;
+    fflush(stdout) ;
+  }
   if ( strcmp(argv[1],"/s") == 0 ) 
   {
   int cdrc = chdir("C:\\Program Files\\QemuSaver") ;
-//  printf("cd returns %d errno=%d\n", cdrc, errno) ; 
-//  fflush(stdout) ; 
+  if ( k_Debug ) {
+    printf("cd returns %d errno=%d\n", cdrc, errno) ;
+    fflush(stdout) ;
+  }
   strcpy(buffer,argv[0]) ;
   int l=strlen(buffer) ;
   while(l > 0 && buffer[l] != '.' )
@@ -25,12 +31,15 @@ int main(int argc, char **argv)
   buffer[l] = 0 ;
   while(l > 0 && buffer[l] != '\\' ) l -= 1 ;
   char cmd[1000] ;
-  sprintf(cmd,"qemu-system-x86_64 -cdrom %s.iso",buffer+l+1) ;
-//  printf("Command is %s\n",cmd) ;
-//  fflush(stdout) ;
+  sprintf(cmd,"qemu-system-x86_64 -cdrom %s.iso -m 1024 -usb -device usb-tablet",buffer+l+1) ;
+  if ( k_Debug ) {
+    printf("Command is %s\n",cmd) ;
+    fflush(stdout) ;
+  }
   int sysrc=system(cmd) ; 
-//  printf("system returns %d errno=%d\n", sysrc, errno) ; 
-//  fflush(stdout) ; 
+  if ( k_Debug ) {
+    printf("system returns %d errno=%d\n", sysrc, errno) ;
+    fflush(stdout) ;
   }
   }
   return 0 ;
