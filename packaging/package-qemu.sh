fe09471a3f91f5aa64d57f5bf612ce74312ba3c7
Chris Ward <tjcw@cantab.net>
2020-02-26 10:53:54 +0000

    fixing

--- packaging/package-qemu.sh
+++ packaging/package-qemu.sh
@@ -1,9 +1,6 @@
 #!/bin/bash 
 mkdir qemu
 cp -r "/cygdrive/c/Program Files/qemu/." qemu
-mv qemu/qemu-system-x86_64.exe  qemu/keep.qemu-system-x86_64.exe
-rm qemu/qemu-system-*.exe
-mv qemu/keep.qemu-system-x86_64.exe qemu/qemu-system-x86_64.exe
 cat skeleton.iss
 cd qemu
 find . -type f -print |
