f0515edc09c100c47d59201bdc2941048f248d0c
Chris Ward <tjcw@cantab.net>
2020-02-26 18:51:40 +0000

    package current open source version of QemuSaver

---
+++ packaging/package-qemu-open.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+#mkdir qemu
+#cp -r "/cygdrive/c/Program Files/qemu/." qemu
+cat skeleton.iss
+cd qemu
+find . -type f -print |
+while read name
+do
+  echo "Source: \"{src}\\qemu\\${name}\"; DestDir: \"{app}\\${name%/*}\"; Flags: ignoreversion external"
+done
+echo "Source: \"{src}\\extras\\fr2.iso\"; DestDir: \"{app}\\..\\QemuSaver\"; Flags: ignoreversion external"
+echo "Source: \"{src}\\extras\\fr2.scr\"; DestDir: \"{sys}\"; Flags: ignoreversion external"
+echo "; NOTE: Don't use "Flags: ignoreversion" on any shared system files"
+
